d44ccfc54066e76e8f3bf29c183dc4c8c1b2b0dd
MD Harunar Rashid <harunarrashid4093@gmail.com>
2025-08-29 18:24:39 +0600

    commit 4rth time

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const callBtn = document.querySelectorAll('.call-btn');
                 alert('the service name and number');
             }
           
-
             else if(counters < 20){
                 alert('আপনার পর্যাপ্ত কয়েন নাই! কমপক্ষে ২০ টি কয়েন লাগবে');
             }
@@ -32,22 +31,69 @@ const callBtn = document.querySelectorAll('.call-btn');
 
         })
     }
-// history icon 
-  
+
+
+
+    let counterCopy = 0;
+    const copyElement = document.getElementById('counter-copy');
+    const copyBtn = document.querySelectorAll('.copy-btn');
+    const numbers = document.querySelectorAll('.number-copy');
+
+        for(const copy of copyBtn){
+            
+            copy.addEventListener("click", function() {
+                counterCopy++;
+                
+                copyElement.textContent = counterCopy;
+
+                navigator.clipboard.writeText(numbers).then(() => {
+                    alert(`number copy: ${numbers}`)
+                })
+            })
+        }
+    
+
     const callButtons = document.querySelectorAll(".call");
-   
     const historyList = document.getElementById("historyList");
+
     callButtons.forEach(btn => {
       btn.addEventListener("click", () => {
         const cardTitle = btn.closest(".card").querySelector("h3").textContent;
-        const cardTitle2 = btn.closest(".card").querySelector("h4").textContent;
+        const callNumber = btn.closest(".card").querySelector("h4").textContent;
         const time = new Date().toLocaleString();
 
         const entry = document.createElement("div");
         entry.className = "entry";
+
+        const title = document.createElement("span");
+        title.className = "entry-title";
+        title.textContent = `${cardTitle}  `;
+
+        const title2 = document.createElement("span");
+        title2.className = "entry-title2";
+        title2.textContent = ` ${callNumber}`;
+
+        const t = document.createElement("span");
+        t.className = "time";
+        t.textContent = ` ${time}`;
         
-        entry.textContent = `${cardTitle} - ${cardTitle2} - ${time}`;
-        
+
+           entry.style.backgroundColor = '#fafafa';
+           entry.style.padding = '10px'
+           entry.style.borderRadius = '10px'
+           entry.style.marginTop = '10px';
+            
+
+        entry.appendChild(title);
+        entry.appendChild(title2);
+        entry.appendChild(t);
+      
+
         historyList.prepend(entry);
+
       });
     });
+
+    document.getElementById('clearBtn').addEventListener('click', function(){
+        historyList.innerHTML = "";
+    })
