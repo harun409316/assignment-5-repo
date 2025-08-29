4d3a69506d2666d67059cd6d3edc7c9084f9910b
MD Harunar Rashid <harunarrashid4093@gmail.com>
2025-08-29 15:57:07 +0600

    my second commit

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log('amar name harun');
+// heart icon 
+
+let counter = 0;
+const counterElement = document.getElementById('counter');
+const heartElement = document.querySelectorAll('.heart');
+
+
+    for(const heart of heartElement){
+        heart.addEventListener("click", function() {
+            counter++;
+            counterElement.textContent = counter;
+        })
+    }
+//  heart icon end 
+let counters = 100;
+const counterElementValue = document.getElementById('counters');
+const callBtn = document.querySelectorAll('.call-btn');
+
+
+    for(let calls of callBtn ){
+        calls.addEventListener('click', function(){
+            counters -= 20;
+            if(counters > 19){
+                alert('the service name and number');
+            }
+          
+
+            else if(counters < 20){
+                alert('আপনার পর্যাপ্ত কয়েন নাই! কমপক্ষে ২০ টি কয়েন লাগবে');
+            }
+
+            counterElementValue.textContent = counters;
+
+        })
+    }
+
+    // প্রতিটি বাটনের জন্য ইভেন্ট যোগ
+    const callButtons = document.querySelectorAll(".call");
+   
+    const historyList = document.getElementById("historyList");
+
+    // Call বাটন ক্লিক করলে হিস্ট্রিতে যোগ হবে
+    callButtons.forEach(btn => {
+      btn.addEventListener("click", () => {
+        const cardTitle = btn.closest(".card").querySelector("h3").textContent;
+        const cardTitle2 = btn.closest(".card").querySelector("h4").textContent;
+        const time = new Date().toLocaleString();
+
+        const entry = document.createElement("div");
+        entry.className = "entry";
+        
+        entry.textContent = `${cardTitle} - ${cardTitle2} - ${time}`;
+       
+        
+        historyList.prepend(entry);
+      });
+    });
